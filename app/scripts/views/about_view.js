a53f0110fb69a1abbf1abff23b21152b74c1beb4
Amalia Viti <amalia.viti@gmail.com>
2013-09-12 23:53:03 -0400

    fixed turnup/ember implementations

--- app/scripts/views/about_view.js
+++ app/scripts/views/about_view.js
@@ -1,20 +1,26 @@
 Amalia.AboutView = Ember.View.extend({
 	didInsertElement: function(event){
-		$('.turnup').turnup({
-			list: [
-				'a nerd',
-				'crazy',
-				'a dj',
-				'a musician',
-				'an engineer',
-				'a developer',
-				'creative',
-				'a maker',
-				'a technologist',
-				'a hacker',
-				'a tinkerer',
-				'data-obsessed'
-			]
-		});
+		if(this.$().find('.turnup').length > 0) {
+			this.$('.turnup').turnup({
+				list: [
+					'a nerd',
+					'crazy',
+					'a dj',
+					'a musician',
+					'an engineer',
+					'a developer',
+					'creative',
+					'a maker',
+					'a technologist',
+					'a hacker',
+					'a tinkerer',
+					'data-obsessed'
+				]
+			});
+		} else {
+			Em.run.next(this, function(){
+				this.didInsertElement();
+			});
+		}
 	}
 });
--- app/scripts/views/project_view.js
+++ app/scripts/views/project_view.js
@@ -1,5 +1,10 @@
 Amalia.ProjectView = Ember.View.extend({
 	didInsertElement: function(event){
+		
 		$('.post').fitVids({ maxWidth: 650 });
+
+		if(this.get('controller').get('id') === 'turnup-js') {
+			this.$('.turnup').turnup({list: ['Grey Wind', 'Lady', 'Nymeria', 'Summer', 'Shaggy Dog']});
+		}
 	}
 });
